b91ab4e7313738a4c93bdb874314108328681e6a
marcelo paschoal <marcelo.hlp@outlook.com>
2025-05-13 18:55:53 -0300

    feat: secao flex direction

--- script/script.js
+++ script/script.js
@@ -1,1 +1,21 @@
-document.writeln("Flexbox é uma poderosa ferramenta de layout no CSS.");
+const divFlexDirection = document.getElementById("div-flex-direction");
+const row = document.getElementById("flex-direction-row");
+const rowReverse = document.getElementById("flex-direction-row-reverse");
+const column = document.getElementById("flex-direction-column");
+const columnReverse = document.getElementById("flex-direction-column-reverse");
+
+row.addEventListener("click", () => {
+    divFlexDirection.style.flexDirection = "row";
+});
+
+rowReverse.addEventListener("click", () => {
+    divFlexDirection.style.flexDirection = "row-reverse";
+});
+
+column.addEventListener("click", () => {
+    divFlexDirection.style.flexDirection = "column";
+});
+
+columnReverse.addEventListener("click", () => {
+    divFlexDirection.style.flexDirection = "column-reverse";
+});
