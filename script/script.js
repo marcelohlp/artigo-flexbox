f58a8ec85c10a78dea85815763c698362dc44faf
marcelo paschoal <marcelo.hlp@outlook.com>
2025-05-14 11:38:15 -0300

    style: definindo o padrao visual da pagina

--- script/script.js
+++ script/script.js
@@ -1,4 +1,5 @@
 const divFlexDirection = document.getElementById("div-flex-direction");
+const spanFlexDirection = document.getElementById("span-flex-direction");
 const row = document.getElementById("flex-direction-row");
 const rowReverse = document.getElementById("flex-direction-row-reverse");
 const column = document.getElementById("flex-direction-column");
@@ -6,16 +7,20 @@ const columnReverse = document.getElementById("flex-direction-column-reverse");
 
 row.addEventListener("click", () => {
     divFlexDirection.style.flexDirection = "row";
+    spanFlexDirection.textContent = "row";
 });
 
 rowReverse.addEventListener("click", () => {
     divFlexDirection.style.flexDirection = "row-reverse";
+    spanFlexDirection.textContent = "row-reverse";
 });
 
 column.addEventListener("click", () => {
     divFlexDirection.style.flexDirection = "column";
+    spanFlexDirection.textContent = "column";
 });
 
 columnReverse.addEventListener("click", () => {
     divFlexDirection.style.flexDirection = "column-reverse";
+    spanFlexDirection.textContent = "column-reverse";
 });
